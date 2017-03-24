7812c918d97d930af74b8488ff0e82e761e24ae2
Estudiantes <Estudiantes@labing50201.udistrital.edu.co>
2017-03-24 17:58:40 -0500

    final

--- app/src/calculadora/presentacion.java
+++ app/src/calculadora/presentacion.java
@@ -24,6 +24,9 @@ Calculadora miCalculadora= new Calculadora();
         cot = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -53,10 +56,46 @@ Calculadora miCalculadora= new Calculadora();
         jLabel3.setText("RESULTADO");
 
         cot.setText("cotangente");
+        cot.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                cotMouseClicked(evt);
+            }
+        });
 
         jButton2.setText("secante");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
 
         jButton3.setText("cosecante");
+        jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton3MouseClicked(evt);
+            }
+        });
+
+        jButton1.setText("asen");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
+
+        jButton4.setText("acos");
+        jButton4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton4MouseClicked(evt);
+            }
+        });
+
+        jButton5.setText("atan");
+        jButton5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton5MouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -76,15 +115,22 @@ Calculadora miCalculadora= new Calculadora();
                         .addGap(55, 55, 55)
                         .addComponent(UNO, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(27, 27, 27)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jButton2)
-                            .addComponent(tangente)
-                            .addComponent(cos)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(sen)
-                                .addGap(47, 47, 47)
-                                .addComponent(jButton3))
-                            .addComponent(cot))))
+                                .addComponent(cot)
+                                .addGap(18, 18, 18)
+                                .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(sen)
+                                    .addComponent(cos)
+                                    .addComponent(tangente))
+                                .addGap(25, 25, 25)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))))
                 .addContainerGap(145, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -97,18 +143,24 @@ Calculadora miCalculadora= new Calculadora();
                     .addComponent(jLabel1)
                     .addComponent(jButton3))
                 .addGap(20, 20, 20)
-                .addComponent(cos)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cos)
+                    .addComponent(jButton1))
                 .addGap(18, 18, 18)
-                .addComponent(tangente)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(cot)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tangente)
+                    .addComponent(jButton4))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(cot)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jLabel3)
                         .addGap(78, 78, 78))
                     .addGroup(layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGap(19, 19, 19)
+                        .addComponent(jButton5)
+                        .addGap(3, 3, 3)
                         .addComponent(jButton2)
                         .addGap(18, 18, 18)
                         .addComponent(RESUL, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -133,6 +185,36 @@ Calculadora miCalculadora= new Calculadora();
      RESUL.setText(miCalculadora.tangente()+"");
     }//GEN-LAST:event_tangenteMouseClicked
 
+    private void cotMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_cotMouseClicked
+       miCalculadora.num1=Double.parseDouble(UNO.getText());
+     RESUL.setText(miCalculadora.cotangente()+"");
+    }//GEN-LAST:event_cotMouseClicked
+
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+miCalculadora.num1=Double.parseDouble(UNO.getText());
+     RESUL.setText(miCalculadora.csc()+"");        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2MouseClicked
+
+    private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
+miCalculadora.num1=Double.parseDouble(UNO.getText());
+     RESUL.setText(miCalculadora.sec()+"");        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3MouseClicked
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+       miCalculadora.num1=Double.parseDouble(UNO.getText());
+     RESUL.setText(miCalculadora.aseno()+"");
+    }//GEN-LAST:event_jButton1MouseClicked
+
+    private void jButton4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton4MouseClicked
+         miCalculadora.num1=Double.parseDouble(UNO.getText());
+     RESUL.setText(miCalculadora.acoseno()+"");
+    }//GEN-LAST:event_jButton4MouseClicked
+
+    private void jButton5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton5MouseClicked
+         miCalculadora.num1=Double.parseDouble(UNO.getText());
+     RESUL.setText(miCalculadora.atangente()+"");
+    }//GEN-LAST:event_jButton5MouseClicked
+
     
     public static void main(String args[]) {
      
@@ -148,8 +230,11 @@ Calculadora miCalculadora= new Calculadora();
     private javax.swing.JTextField UNO;
     private javax.swing.JButton cos;
     private javax.swing.JButton cot;
+    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JButton sen;
